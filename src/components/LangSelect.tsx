b0146a1528a91e71debca750ddbb43a3e4772146
Uptaker <markus.tammeoja@gmail.com>
2022-11-13 19:45:58 +0200

    simplify LangSelect & get rid of redundant reactive variables - they are not needed due to the page reload anyway

--- src/components/LangSelect.tsx
+++ src/components/LangSelect.tsx
@@ -1,22 +1,15 @@
 import {lang, langs, rememberLang} from '@codeborne/i18n-json'
-import {useState} from 'react'
 
 function LangSelect() {
 
-  const [language, setLanguage] = useState(lang);
-
   function handleLangSelect(lang: string) {
-    console.log(lang)
-    setLanguage(lang)
     rememberLang(lang)
     location.reload()
   }
 
   return (
-    <select value={language} onChange={e => handleLangSelect(e.target.value)}>
-      {langs.map(lang => {
-        return <option key={lang} value={lang}>{lang.toUpperCase()}</option>
-      })}
+    <select value={lang} onChange={e => handleLangSelect(e.target.value)}>
+      {langs.map(lang => <option key={lang} value={lang}>{lang.toUpperCase()}</option>)}
     </select>
   )
 }
