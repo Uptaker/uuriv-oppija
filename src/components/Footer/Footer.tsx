646153e0630b659be645c2e9f9a14fe4af3f8351
Donsirsimon <kenny85@tlu.ee>
2022-11-20 19:06:05 +0200

    working nav and footer initial

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/ELU-Stamp.svg'
+const Footer = () => {
+  return (
+    <div className="footer"> 
+      <div className='info-container'>        
+        <Link to='/'className='logo'>
+          Uuriv õppija
+        </Link>
+        <div>Siia märgime vajadusel kontaktandmed</div>
+        <div>Copyright 2022 © Uuriv õppija</div>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="Erialasid lõimiv Uuendus" />
+      </div>  
+    </div>
+    
+  )
+}
+
+export default Footer
---
+++ src/pages/Vahelduvope.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Vahelduvope = () => {
+  return (
+    <div>Vahelduvope</div>
+  )
+}
+
+export default Vahelduvope
