9faa8a6a7af1d81b4e05a326d1d770c1d5b9d17b
Uptaker <markus.tammeoja@gmail.com>
2022-12-06 17:53:36 +0200

    add generic interface for each strategy

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type Option = {
+    label: string;
+    value: string | number;
+}
+
+export interface Strategy {
+    title: string,
+    icon: string,
+    content: StrategyContent,
+    apps: StrategyUsefulApp[]
+    videoUrl?: string
+}
+
+export interface StrategyContent {
+    what: string
+    why: string,
+    how: string
+}
+
+export interface StrategyUsefulApp {
+    name: string,
+    description: string
+    url: string
+}
