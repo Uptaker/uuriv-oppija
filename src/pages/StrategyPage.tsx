a4b9be06f49e481fcd3ff778a3a12e960528408d
Uptaker <markus.tammeoja@gmail.com>
2022-12-06 20:14:16 +0200

    use the same video frame height/width as in Figma

--- src/pages/StrategyPage.tsx
+++ src/pages/StrategyPage.tsx
@@ -14,14 +14,14 @@ function StrategyPage() {
   return !strategy ? (<>TODO generic not found page</>) : (
     <>
       <div style={{maxWidth: "1299px", margin: "0 auto"}}>
-        <h1>{_(strategy.title)}</h1>
+        <h1 style={{margin: "4rem 0"}}>{_(strategy.title)}</h1>
         <StrategyInfoCard color={strategy.color} label={_('general.what')} content={_(strategy.content.what)}/>
         <StrategyInfoCard color={strategy.color} label={_('general.why')} content={_(strategy.content.why)}/>
         <StrategyInfoCard color={strategy.color} label={_('general.how')} content={_(strategy.content.how)}/>
       </div>
 
       <div style={{backgroundColor: strategy.color, display: "flex", justifyContent: "center", padding: "51px 10px"}}>
-        <iframe width="560" height="315" src={strategy.videoUrl ? strategy.videoUrl : "https://www.youtube.com/embed/dQw4w9WgXcQ"} title="YouTube video player"
+        <iframe width="812" height="424" src={strategy.videoUrl ? strategy.videoUrl : "https://www.youtube.com/embed/dQw4w9WgXcQ"} title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen/>
