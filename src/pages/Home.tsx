8ed0486d7ba0dd14a7bb567a28665e66a3a82470
Uptaker <markus.tammeoja@gmail.com>
2022-12-06 22:01:05 +0200

    add banner & initial content to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import banner from '../assets/home-banner.svg'
+import {_} from '@codeborne/i18n-json'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div style={{display: "grid", gridTemplateColumns: "1fr 1fr", gap: "1rem", justifyItems: "center"}}>
+        <div style={{display: "flex", justifyContent: "center", flexDirection: "column", gap: "1rem"}}>
+          <h1 className="text-xl">{_('title')}</h1>
+          <div className="text-lg">{_('home.description')}</div>
+          <button className="btn text-md">{_('general.readMore')}</button>
+        </div>
+        <img src={banner} alt="banner"/>
+      </div>
+
+      <div style={{backgroundColor: "#FFEE93", display: "flex", justifyContent: "center", padding: "1rem 1rem", margin: "5rem 0"}}>
+        <h1>{_('home.whyForMe')}</h1>
+      </div>
+    </>
   )
 }
 
