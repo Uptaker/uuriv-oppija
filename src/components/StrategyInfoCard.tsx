54a9d69b533581877082e5fddf8f3ac08c262ad9
Uptaker <markus.tammeoja@gmail.com>
2022-12-06 20:00:39 +0200

    increase font size to match Figma (it's ridiculously big)
    inline css in used as Marilii wants to correct everything to use CSS modules

--- src/components/StrategyInfoCard.tsx
+++ src/components/StrategyInfoCard.tsx
@@ -7,8 +7,8 @@ function StrategyInfoCard({label, content, color}: StrategyInfoCardProps) {
 
   return (
       <div className="strategy-info" style={{borderColor: color}}>
-        <h2 style={{backgroundColor: color, fontWeight: "30px", margin: "0", padding: "30px"}}>{label}</h2>
-        <p style={{padding: "24px", fontWeight: "28px"}}>{content}</p>
+        <h2 style={{backgroundColor: color, fontSize: "30px", margin: "0", padding: "30px"}}>{label}</h2>
+        <p style={{padding: "24px", fontSize: "28px"}}>{content}</p>
       </div>
   )
 }
