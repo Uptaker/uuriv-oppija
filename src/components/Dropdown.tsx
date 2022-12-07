9ade91ebff7b26b4dd0eb95a396d1a604efb4bc9
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-08 01:18:11 +0200

    Rework nav dropdown to use css modules + variables

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,8 +1,10 @@
+import classNames from 'classnames'
 import React, {useState} from 'react'
-import './Dropdown.css'
 import {Link} from 'react-router-dom'
 import {strategies} from '../store/Strategies'
 import {_} from '@codeborne/i18n-json'
+
+import s from './Dropdown.module.scss'
 
 function Dropdown() {
   const [click, setClick] = useState(false);
@@ -11,12 +13,20 @@ function Dropdown() {
 
   return (
     <>
-      <ul onClick={handleClick} className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}>
+      <ul
+        onClick={handleClick}
+        className={classNames({
+          [s.dropdownMenu]: true,
+          'clicked': click,
+        })}
+      >
         {strategies.map(strategy => {
           return (
-            <li key={strategy.id}>
+            <li key={strategy.id} className={s.drowdownItem}>
               <Link
-                className='dropdown-link' to={'/strategy/' + strategy.id} onClick={() => setClick(false)}>
+                className={s.dropdownLink}
+                data-text={_(strategy.title)}
+                to={'/strategy/' + strategy.id} onClick={() => setClick(false)}>
                 {_(strategy.title)}
               </Link>
             </li>
