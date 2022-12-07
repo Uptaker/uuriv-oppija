74a566e9921c890a0b1f0b73c45d574b61c84acb
Uptaker <markus.tammeoja@gmail.com>
2022-12-07 13:05:15 +0200

    add placeholder 'strategy reasons' section on home page

---
+++ src/components/ItemDescription.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface ItemDescriptionProps {
+  title: string,
+  text: string,
+  icon: string
+}
+
+function ItemDescription({title, text, icon}: ItemDescriptionProps) {
+  return (
+    <div style={{display: "flex", margin: "3rem 0", padding: "1rem 2rem", justifyContent: "center", flexDirection: "column", alignItems: "center", flex: "1 1 30%"}}>
+      {icon && <img src={icon} alt="idea" width="90" height="90"/>}
+      {title && <p className="text-md bold" style={{margin: "0.5rem 0"}}>{title}</p>}
+      {text && <p className="text-md" style={{margin: "0.5rem 0", textAlign: "center"}}>{text}</p>}
+    </div>
+  )
+}
+
+export default ItemDescription
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import banner from '../assets/home-banner.svg'
 import {_} from '@codeborne/i18n-json'
+import attitude from '../assets/attitude.svg'
+import improvement from '../assets/improvement.svg'
+import stress from '../assets/stress.svg'
+import confidence from '../assets/confidence.svg'
+import timeSavings from '../assets/timeSavings.svg'
+import ItemDescription from '../components/ItemDescription'
 
 function HomePage() {
   return (
@@ -16,6 +22,14 @@ function HomePage() {
 
       <div style={{backgroundColor: "#FFEE93", display: "flex", justifyContent: "center", padding: "1rem 1rem", margin: "0 0 5rem 0"}}>
         <h1>{_('home.whyForMe')}</h1>
+      </div>
+
+      <div style={{display: "flex", flexWrap: "wrap", gap: "1rem", justifyContent: "center", alignContent: "center"}}>
+        <ItemDescription title={_('home.attitude')} text={_('home.attitudeText')} icon={attitude}/>
+        <ItemDescription title={_('home.improvement')} text={_('home.improvementText')} icon={improvement}/>
+        <ItemDescription title={_('home.stress')} text={_('home.stressText')} icon={stress}/>
+        <ItemDescription title={_('home.confidence')} text={_('home.confidenceText')} icon={confidence}/>
+        <ItemDescription title={_('home.timeSavings')} text={_('home.timeSavingsText')} icon={timeSavings}/>
       </div>
     </>
   )
