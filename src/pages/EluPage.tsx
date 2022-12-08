90d4d1c11849e18fef19fe93769a89ab6c6ecf5e
Uptaker <markus.tammeoja@gmail.com>
2022-12-09 00:24:37 +0200

    add ELU and project description

--- src/pages/EluPage.tsx
+++ src/pages/EluPage.tsx
@@ -7,19 +7,27 @@ import eluEN from '../assets/ELU-Stamp-EN.svg'
 
 import s from './EluPage.module.scss'
 import {participants} from '../store/Participants'
+import classNames from 'classnames'
 
 function EluPage() {
   const lang = document.cookie.split('; ')?.find(s => s?.startsWith('LANG='))?.split('=')?.[1]
 
   return (
     <ContentDiv>
-      <div className={s.container}>
+      <div className={s.grid}>
         <div className={s.title}>
-          <h1>{_('elu.title')}</h1>
-          <div>{_('elu.description')}</div>
+          <h1>{_('elu.eluTitle')}</h1>
+          <div>{_('elu.eluDescription')}</div>
+          <a className={s.source} href="https://elu.tlu.ee" target="_blank">{_('elu.eluSource')} <i className={classNames("fa-solid fa-arrow-up-right-from-square")}></i></a>
         </div>
         <img src={lang === 'en' ? eluEN : elu} className={s.img} alt={_('general.logo')}/>
       </div>
+
+        <div className={s.container}>
+            <h1>{_('elu.projectTitle')}</h1>
+            <div>{_('elu.projectDescription1')}</div>
+            <div>{_('elu.projectDescription2')}</div>
+        </div>
 
       <Container color="peach" label={_('elu.teamTitle')}>
         <ul>
