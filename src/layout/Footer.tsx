09aab248bf96e19d8dcc55e7d8b34b3b2d06a9fb
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-08 12:12:08 +0200

    Add translations to footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../assets/ELU-Stamp.svg'
+import logoEN from '../assets/ELU-Stamp-EN.svg'
 import LangSelect from '../components/LangSelect'
 import {_} from '@codeborne/i18n-json'
 
 import s from './Footer.module.scss'
 
 function Footer() {
+  const lang = document.cookie.split('; ')?.find(s => s?.startsWith('LANG='))?.split('=')?.[1]
+
   return (
     <div className={s.footer}>
       <div className='info-container'>
         <p>
           <Link to='/' className={s.title}>
-            Uuriv õppija
+            {_('title')}
           </Link>
         </p>
-        <p>Siia märgime vajadusel kontaktandmed</p>
-        <p>Copyright {(new Date().getFullYear())} © Uuriv õppija</p>
+        <p>{_('nav.contacts')}</p>
+        <p>{_('nav.copyright')} {(new Date().getFullYear())} © {_('title')}</p>
       </div>
       <LangSelect/>
       <div className="logo-container">
-        <img src={logo} alt="Erialasid lõimiv Uuendus"/>
+        <img src={lang === 'en' ? logoEN : logo} alt={_('general.logo')}/>
       </div>
     </div>
 
