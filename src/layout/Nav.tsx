14617426034ca413af8bd90e41c9901eeee66464
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-08 11:33:16 +0200

    Add navbar translations

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import Dropdown from '../components/Dropdown'
+import {_} from '@codeborne/i18n-json'
 
 import s from './Nav.module.scss'
 
@@ -29,43 +30,31 @@ function Nav() {
     <>
       <nav className={s.navbar}>
         <Link to='/' className={classNames(s.navLink, s.navLogo)}>
-          Uuriv õppija
-
+        {_('title')}
         </Link>
 
         <ul className={s.navMenu}>
-
           <li
             className={s.dropdownNav}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
           >
-            <div
-              className={classNames(s.navLink, 'strategies')}
-            >
-              Strateegiad <i className='fas fa-caret-down'/>
+            <div className={classNames(s.navLink, 'strategies')}>
+              {_('nav.strategies')} <i className='fas fa-caret-down'/>
             </div>
             {dropdown && <Dropdown/>}
           </li>
           <li>
-            <Link
-              to='/learn-more'
-              className={s.navLink}
-            >
-              Lisaks
+            <Link to='/learn-more' className={s.navLink}>
+              {_('nav.more')}
             </Link>
           </li>
           <li>
-            <Link
-              to='/elu'
-              className={s.navLink}
-            >
-              ELU
+            <Link to='/elu' className={s.navLink}>
+              {_('nav.ELU')}
             </Link>
           </li>
-
         </ul>
-
       </nav>
     </>
   )
