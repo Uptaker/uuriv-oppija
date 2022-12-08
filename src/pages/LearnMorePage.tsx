1ae5d324bfef930cfe7d0668c090c30bc85a351b
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-08 15:51:45 +0200

    Update LearnMorePage

--- src/pages/LearnMorePage.tsx
+++ src/pages/LearnMorePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './StrategyPage.css'
 import {_} from '@codeborne/i18n-json'
 import Container from '../components/Container'
 import Collapsible from '../components/Collapsible'
@@ -8,7 +7,7 @@ function LearnMorePage() {
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', gap: '4rem', maxWidth: "1299px", margin: "3rem auto"}}>
-      <Container color={"#FFC09F"} label={_('learnMore.readingTitle')}>
+      <Container color="peach" label={_('learnMore.readingTitle')}>
         <Collapsible label={"TODO (or any custom non-collapsible content here)"}>
           <div>TODO </div>
         </Collapsible>
@@ -17,7 +16,7 @@ function LearnMorePage() {
         </Collapsible>
       </Container>
 
-        <Container color="#FFC09F" label={_('learnMore.viewingTitle')}>
+        <Container color="peach" label={_('learnMore.viewingTitle')}>
           <Collapsible label={"TODO"}>
             <div>TODO</div>
           </Collapsible>
