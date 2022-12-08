63c27c113f9329b79b44e0495874de9d3cc425b5
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-08 15:50:45 +0200

    Use modules for EluPage

--- src/pages/EluPage.tsx
+++ src/pages/EluPage.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import './StrategyPage.css'
 import {_} from '@codeborne/i18n-json'
+import ContentDiv from '../layout/ContentDiv'
 import Container from '../components/Container'
 import Collapsible from '../components/Collapsible'
 import elu from '../assets/ELU-Stamp.svg'
+import eluEN from '../assets/ELU-Stamp-EN.svg'
+
+import s from './EluPage.module.scss'
 
 function EluPage() {
+  const lang = document.cookie.split('; ')?.find(s => s?.startsWith('LANG='))?.split('=')?.[1]
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', gap: '4rem', maxWidth: "1299px", margin: "3rem auto"}}>
-      <div style={{display: "grid", gridTemplateColumns: "2fr 1fr", gap: "5rem", justifyItems: "center", alignItems: "center"}}>
-        <div style={{display: "flex", justifyContent: "center", flexDirection: "column", gap: "1rem"}}>
-          <h1 className="text-xl">{_('elu.title')}</h1>
-          <div className="text-lg">{_('elu.description')}</div>
+    <ContentDiv>
+      <div className={s.container}>
+        <div className={s.title}>
+          <h1>{_('elu.title')}</h1>
+          <div>{_('elu.description')}</div>
         </div>
-        <img src={elu} width="350" alt="banner"/>
+        <img src={lang === 'en' ? eluEN : elu} className={s.img} alt={_('general.logo')}/>
       </div>
 
-      <Container color="#FFC09F" label={_('elu.teamTitle')}>
+      <Container color="peach" label={_('elu.teamTitle')}>
         <Collapsible label={"TODO"}>
           <div>TODO</div>
         </Collapsible>
@@ -25,7 +29,7 @@ function EluPage() {
           <div>TODO</div>
         </Collapsible>
       </Container>
-    </div>
+    </ContentDiv>
   )
 }
 
