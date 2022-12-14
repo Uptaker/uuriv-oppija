e95add43fcdfbb599c978e5c05dcfc94fe52c14d
Uptaker <markus.tammeoja@gmail.com>
2022-12-14 02:37:30 +0200

    revert mobile changes in `main`

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -8,53 +8,49 @@ import s from './Nav.module.scss'
 
 function Nav() {
   const [dropdown, setDropdown] = useState(false)
-
-  const onMouseEnter = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false)
-    } else {
-      setDropdown(true)
-    }
-  };
-
-  const onMouseLeave = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false)
-    } else {
-      setDropdown(false)
-    }
-  };
+  const [isOpen, setIsOpen] = useState(false)
 
 
+  // @ts-ignore
   return (
     <nav className={s.navbar}>
       <Link to='/' className={classNames(s.navLink, s.navLogo)}>
       {_('title')}
       </Link>
 
-      <ul className={s.navMenu}>
-        <li
-          className={s.dropdownNav}
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-          
-        >
-          <div className={classNames(s.navLink, 'strategies')}>
-            {_('nav.strategies')} <i className='fas fa-caret-down'/>
-          </div>
-          {dropdown && <Dropdown/>}
-        </li>
-        <li>
-          <Link to='/learn-more' className={s.navLink}>
-            {_('nav.more')}
-          </Link>
-        </li>
-        <li>
-          <Link to='/elu' className={s.navLink}>
-            {_('nav.ELU')}
-          </Link>
-        </li>
-      </ul>
+      <div>
+        <ul className={classNames({[s.navMenu]: true, [s.navOpen]: isOpen})}>
+          <li
+            className={s.dropdown}
+            onMouseEnter={() => setDropdown(true)}
+            onMouseLeave={() => setDropdown(false)}
+            onClick={() => setDropdown(!dropdown)}
+          >
+            <div className={classNames(s.navLink, 'strategies')}>
+              {_('nav.strategies')} <i className='fas fa-caret-down'/>
+            </div>
+            {dropdown && <Dropdown/>}
+          </li>
+          <li>
+            <Link to='/learn-more' className={s.navLink}>
+              {_('nav.more')}
+            </Link>
+          </li>
+          <li>
+            <Link to='/elu' className={s.navLink}>
+              {_('nav.ELU')}
+            </Link>
+          </li>
+        </ul>
+        <div className={classNames({
+          [s.burgerMenu]: true,
+          [s.burgerOpen]: !isOpen,
+          [s.burgerClosed]: isOpen,
+        })}
+         onClick={() => setIsOpen(!isOpen)}/>
+      </div>
+
+
     </nav>
   )
 }
