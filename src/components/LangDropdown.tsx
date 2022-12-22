fb59259906736e44b2da4f32c6e0ac0b7626562b
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-22 20:19:57 +0200

    Remove console log

--- src/components/LangDropdown.tsx
+++ src/components/LangDropdown.tsx
@@ -3,46 +3,35 @@ import React, {useState} from 'react'
 import {lang, langs, rememberLang} from '@codeborne/i18n-json'
 import s from './LangDropdown.module.scss'
 
-
 function LangDropdown() {
-    
+  const [click, setClick] = useState(false); 
+  const handleClick = () => setClick(!click);
 
-    const [click, setClick] = useState(false);
-  
-    const handleClick = () => setClick(!click);
-
-    function handleLangSelect(lang: string) {
-        rememberLang(lang)
-        location.reload()
-    }
-    
-    console.log(lang)
-    for(let i=0; i<2; i++){
-        console.log(langs[i])
-    }
-    
-    return (
-        <>
-        
-          <ul
-            onClick={handleClick}
-            className={classNames({
-              [s.langDropdownMenu]: true,
-              'clicked': click,
-            })}
-          >
-            {langs.map(e => {
-              return (
-                <li key={e} value={e} className={classNames(s.langDrowdownItem, s.langDropdownLink, {[s.langItemHighlighted]: e==lang} )}   onClick={() => handleLangSelect(e)} >
-               
-                    {e.toUpperCase()}
-                  
-                </li>
-              );
-            })}
-          </ul>
-        </>
-      );
+  function handleLangSelect(lang: string) {
+      rememberLang(lang)
+      location.reload()
   }
+  
+  return (
+    <ul
+      onClick={handleClick}
+      className={classNames({
+        [s.langDropdownMenu]: true,
+        'clicked': click,
+      })}
+    >
+      {langs.map(e => (
+          <li
+            key={e}
+            value={e}
+            className={classNames(s.langDrowdownItem, s.langDropdownLink, {[s.langItemHighlighted]: e === lang} )}
+            onClick={() => handleLangSelect(e)}>               
+              {e.toUpperCase()}
+          </li>
+        )
+      )}
+    </ul>
+  );
+}
   
   export default LangDropdown;
