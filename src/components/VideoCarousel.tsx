7dc78b4bec3deff8f2286810d4ac3e4164d83c01
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-24 01:50:31 +0200

    Display different amount of videos in carousel

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -1,14 +1,41 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {strategies} from '../store/Strategies'
 import ResponsiveIframeVideo from './ResponsiveIframeVideo'
 import classNames from 'classnames'
 import s from './VideoCarousel.module.scss'
 
-
 function VideoCarousel() {
-
   const videos = strategies.map(s => s.videoUrl) as string[]
   const [queue, setQueue] = useState(videos)
+  const [isDesktop, setIsDektop] = useState(false)
+  const [isTablet, setIsTablet] = useState(false)
+
+  // Using media queries within React: https://stackoverflow.com/a/66590903
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia('(min-width: 1024px)')
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsDektop(e.matches)
+    }
+
+    mediaQueryList.addEventListener('change', handleChange)
+
+    return () => {
+      mediaQueryList.removeEventListener('change', handleChange)
+    }
+  }, [])
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia('(min-width: 768px)')
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsTablet(e.matches)
+    }
+
+    mediaQueryList.addEventListener('change', handleChange)
+
+    return () => {
+      mediaQueryList.removeEventListener('change', handleChange)
+    }
+  }, [])
 
   function next() {
     queue.unshift(queue.pop() as string)
@@ -20,14 +47,13 @@ function VideoCarousel() {
     setQueue([...queue])
   }
 
-
   return (
     <div className={classNames(s.container)}>
       <i className={classNames('fa-solid fa-chevron-left', s.pointer)} onClick={() => back()}></i>
       <div className={classNames(s.videos)}>
         <ResponsiveIframeVideo key={queue[videos.length - 1]} url={queue[videos.length - 1]}/>
-        <ResponsiveIframeVideo key={queue[0]} url={queue[0]}/>
-        <ResponsiveIframeVideo key={queue[1]} url={queue[1]}/>
+        {isTablet && <ResponsiveIframeVideo key={queue[0]} url={queue[0]}/>}
+        {isDesktop && <ResponsiveIframeVideo key={queue[1]} url={queue[1]}/>}
       </div>
       <i className={classNames('fa-solid fa-chevron-right', s.pointer)} onClick={() => next()}></i>
     </div>
