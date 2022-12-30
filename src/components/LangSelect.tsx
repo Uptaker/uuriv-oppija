8eeecc74b5ae836710f48067a086bba7f465c04d
Neniariel <42422684+Neniariel@users.noreply.github.com>
2022-12-31 01:41:26 +0200

    Remove comment

--- src/components/LangSelect.tsx
+++ src/components/LangSelect.tsx
@@ -8,7 +8,6 @@ function LangSelect() {
   }
 
   return (
-    // <i className="fa-regular fa-globe"></i> TODO use this icon for lang select
     <select value={lang} onChange={e => handleLangSelect(e.target.value)}>
       {langs.map(lang => <option key={lang} value={lang}>{lang.toUpperCase()}</option>)}
     </select>
