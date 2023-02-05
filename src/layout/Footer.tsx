62da6e9d2e4b39ea7c5b5eb4bbcae81fb7883b1f
Marilii Saar <ly@marilii.com>
2023-02-06 00:28:36 +0200

    Add Illumine logo + text

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../assets/ELU-Stamp.svg'
 import logoEN from '../assets/ELU-Stamp-EN.svg'
+import illumine from '../assets/illumine.png'
 import {_} from '@codeborne/i18n-json'
 
 import s from './Footer.module.scss'
@@ -11,20 +12,21 @@ function Footer() {
 
   return (
     <div className={s.footer}>
-      <div className='info-container'>
+      <div className={s.illumine}>
         <p>
           <Link to='/' className={s.title}>
             {_('title')}
           </Link>
         </p>
         <p><a href="mailto:merike.saar@tlu.ee"><i className="fa-regular fa-envelope"/>  merike.saar@tlu.ee</a></p>
+        <p className={s.copyright}>{_('nav.illumine')}</p>
+        <a href="https://illumine.upf.edu/" target="_blank"><img src={illumine} /></a>
         <p className={s.copyright}>{_('nav.copyright')} {(new Date().getFullYear())} © {_('title')}</p>
       </div>
-      <div className="logo-container">
+      <div>
         <img src={lang === 'en' ? logoEN : logo} alt={_('general.logo')}/>
       </div>
     </div>
-
   )
 }
 
